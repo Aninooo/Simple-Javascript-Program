24747c705d9a5f60ecb46436541af37bcf8e4818
Ani no <bryanlomerioanino@gmail.com>
2024-08-22 09:49:09 +0800

    added new functions for random number generator

--- random number generator/random-number-generator.js
+++ random number generator/random-number-generator.js
@@ -1,11 +1,19 @@
 const myButton = document.getElementById('myButton');
 const label = document.getElementById('label');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
 
 const min = 1;
-const max = 1000;
+const max = 20;
 let randomNum;
+let randomNum2;
+let randomNum3;
 
 myButton.onclick = function(){
     randomNum = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
     label.textContent = randomNum;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
 }
