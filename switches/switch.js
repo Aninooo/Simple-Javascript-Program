d7ebffd22e27937ee8de4110ef937d6bd255a8de
Ani no <bryanlomerioanino@gmail.com>
2024-08-22 12:48:06 +0800

    added switch case

---
+++ switches/switch.js
@@ -0,0 +1,17 @@
+let day = 10;
+
+switch(day){
+    case 1: 
+    console.log('It is monday');
+    break;
+
+    case 2:
+        console.log('It is Tuesday');
+        break;
+    
+    case 3:
+        console.log('It is Monday');
+
+    default:
+        console.log(`${day} It is not a day`)
+}
